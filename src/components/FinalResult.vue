2569629062f0f6715863b00d54d23ed614c8d7d1
Max Liksunov <mliksunov@magecom.net>
2022-05-08 16:44:20 +0300

    main component results

--- src/components/FinalResult.vue
+++ src/components/FinalResult.vue
@@ -3,14 +3,26 @@
     clickable
     tag="a"
     target="_blank"
+    style="align-items: flex-start"
     :href="link"
   >
-    {{final.label}}
+    <q-item-section>
+      <h5 style="margin-top: 0">{{final.label}}</h5>
+      <ul style="padding: 0">
+        <li style="list-style-position: inside;" v-for="(item, index) in info.tth" :key="index">
+          {{item}}
+        </li>
+      </ul>
+    </q-item-section>
+    <q-item-section>
+      <p>{{info.description}}</p>
+    </q-item-section>
   </q-item>
 </template>
 
 <script lang="ts">
 import { defineComponent, toRaw, computed } from 'vue';
+import {Describer} from '../describer/describer'
 
 export default defineComponent({
   name: 'FinalResult',
@@ -21,10 +33,12 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const final = computed(() => toRaw(props.result).filter(i => (i as any).confidence === Math.max(...props.result.map(o => (o as any).confidence)))[0])
+    const final = computed(() => toRaw(props.result).filter(i => (i as any).confidence === Math.max(...props.result.map(o => (o as any).confidence)))[0]) as any
+    const info = computed(() => new Describer().getInfo(final.value.label as any))
 
     return {
-      final
+      final,
+      info
     }
   }
 });
