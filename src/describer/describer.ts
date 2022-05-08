cc6eff306557703814fe26d8113cb4235d8b85cb
Yurii Shtyba <yurii.shtyba@globallogic.com>
2022-05-08 16:10:16 +0300

    chng describer

--- src/describer/describer.ts
+++ src/describer/describer.ts
@@ -2,7 +2,15 @@ import { ShellInfo, TTH } from './tth';
 
 export class Describer {
   constructor() {
-    TTH.forEach((el) => this.info.set(el.name, el));
+    TTH.forEach((el) => this._info.set(el.name, el));
   }
-  public readonly info: Map<string, ShellInfo> = new Map<string, ShellInfo>();
+  private readonly _info: Map<string, ShellInfo> = new Map<string, ShellInfo>();
+
+  public getInfo(name: string): ShellInfo {
+    const result = this._info.get(name);
+    if (!result) {
+      throw new Error();
+    }
+    return result;
+  }
 }
